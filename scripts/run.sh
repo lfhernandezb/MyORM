54dcdbe2450558726f2abb9ffd0b612498fce200
DSOFT EIRL <lfhernandez@dsoft.cl>
2014-04-05 17:34:51 -0300

    version inicial

---
+++ scripts/run.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+##############################################################################
+#
+PROGRAM_PATH=`dirname $0`
+
+#JAVA_HOME=/opt/jdk1.6.0_38/
+JAVA_HOME=/opt/jdk1.6.0_27
+TMP_DIR=/var/tmp
+CLASSPATH=\
+$JAVA_HOME/lib/tools.jar:
+CLASSPATH=\
+$PROGRAM_PATH/../bin/MyORM.jar
+
+# seteando el classpath
+. $PROGRAM_PATH/env.sh
+
+echo $CLASSPATH
+#
+# Start program
+#
+$JAVA_HOME/bin/java \
+-Djava.io.tmpdir=$TMP_DIR \
+-Dlog4j.debug=true \
+-Dlog4j.configuration=file:$PROGRAM_PATH/../etc/log4j.xml \
+-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.Log4JLogger \
+-Dconfig_file=$PROGRAM_PATH/../etc/config.ini \
+-cp `cygpath -wp $CLASSPATH` \
+MyORM
+
+#$JAVACMD LoadCaf $@
